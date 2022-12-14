fd4cf75dc3dd456894a53a3e33b1775fe4068ff3
Pedro <pedropaivadev@gmail.com>
2022-12-14 10:22:21 -0300

    setup completed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
-import { MailService } from './mail/mail.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { Body } from '@nestjs/common/decorators';
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.mailService.sendEmail();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      }
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { HttpModule } from './mail/http.module';
-// import { AppService } from './app.service';
-import { MailService } from './mail/mail.service';
-import { PostmarkMailService } from './mail/postmark-mail.service';
-import { SMTPMailService } from './mail/smtp-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [
-    HttpModule
-  ],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [
-    {
-      provide: MailService,
-      useClass: PostmarkMailService,
-    }
-  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
