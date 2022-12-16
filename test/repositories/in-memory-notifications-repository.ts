2b5d278f9134be81d544620fbe6cef8d48bf6d6e
Pedro <pedropaivadev@gmail.com>
2022-12-16 11:39:42 -0300

    finished the project

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -2,6 +2,7 @@ import { Notification } from "@application/entities/notification";
 import { NotificationsRepository } from "@application/repositories/notifications-repository";
 
 export class InMemoryNotificationsRepository implements NotificationsRepository {
+    
     public notifications: Notification[] = [];
     
     async findById(notificationId: string): Promise<Notification | null> {
@@ -12,6 +13,18 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
             return null;
         }
         return notification;
+    }
+
+    async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        return this.notifications.filter(
+            notification => notification.recipientId === recipientId
+        );
+    }
+
+    async countManyByRecipientId(recipientId: string): Promise<number> {
+        return this.notifications.filter(
+            notification => notification.recipientId === recipientId
+        ).length;
     }
 
     async create(notification: Notification) {
